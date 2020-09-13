3a2749a445f21baae9de7c29b02fe7496b57d806
akinbowale <akinbowale.akt@gmail.com>
2020-09-14 00:09:30 +0100

    two sum

---
+++ two_sum_1.py
@@ -0,0 +1,43 @@
+# Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+# You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+# You can return the answer in any order.
+
+
+
+# Example 1:
+
+# Input: nums = [2,7,11,15], target = 9
+# Output: [0,1]
+# Output: Because nums[0] + nums[1] == 9, we return [0, 1].
+# Example 2:
+
+# Input: nums = [3,2,4], target = 6
+# Output: [1,2]
+# Example 3:
+
+# Input: nums = [3,3], target = 6
+# Output: [0,1]
+
+
+# Constraints:
+
+# 2 <= nums.length <= 105
+# -109 <= nums[i] <= 109
+# -109 <= target <= 109
+# Only one valid answer exists.
+
+from typing import List
+
+
+def two_sum(nums: List[int], target: int) -> List[int]:
+    lookup_table = {}
+
+    for i, num in enumerate(nums):
+        second_number = target - num
+
+        if second_number not in lookup_table:
+            lookup_table[num] = i
+        else:
+            return [i, lookup_table[second_number]]
