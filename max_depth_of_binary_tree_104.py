35588d58bdacb5a180813190ee5e59569669d1ac
akinbowale <akinbowale.akt@gmail.com>
2020-09-16 22:58:57 +0100

    add solution 2

--- max_depth_of_binary_tree_104.py
+++ max_depth_of_binary_tree_104.py
@@ -23,8 +23,30 @@ class TreeNode:
         self.right = right
 
 
+# Solution 1 - DFS
 def max_depth(root: TreeNode) -> int:
     if root is None:
         return 0
 
     return 1 + max(max_depth(root.left), max_depth(root.right))
+
+
+# Solution 2 - DFS
+def max_depth(root: TreeNode) -> int:
+    if not root:
+        return 0
+
+    queue = [root]
+    depth = 0
+
+    while queue:
+        size = len(queue)
+        depth += 1
+        while size > 0:
+            item = queue.pop(0)
+            if item.left:
+                queue.append(item.left)
+            if item.right:
+                queue.append(item.right)
+            size -= 1
+    return depth
