de65a816c35e6d29aeedbacfd49d34a570ed00eb
Akinbowale Akin-Taylor <akinbowale.akt@gmail.com>
2022-08-09 21:18:43 +0100

    Time: 52 ms (89.42%), Space: 14.4 MB (67.19%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.py
+++ 242-valid-anagram/242-valid-anagram.py
@@ -1,3 +1,21 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        return sorted(s) == sorted(t)
+        hashmap = {}
+        
+        if len(s) != len(t):
+            return False
+        
+        for char in s:
+            if char in hashmap:
+                hashmap[char] += 1
+            else:
+                hashmap[char] = 0
+                
+        for char in t:
+            if char in hashmap:
+                if hashmap[char] == 0:
+                    hashmap.pop(char)
+                else:
+                    hashmap[char] -= 1
+        
+        return True if len(hashmap) == 0 else False
