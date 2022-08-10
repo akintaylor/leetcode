bd7c2cbb832a6469ac8f58cb1f32bd1f95ee20c6
Akinbowale Akin-Taylor <akinbowale.akt@gmail.com>
2022-08-10 16:46:48 +0100

    Time: 61 ms (96.59%), Space: 15.2 MB (50.50%) - LeetHub

--- 1-two-sum/1-two-sum.py
+++ 1-two-sum/1-two-sum.py
@@ -1,11 +1,12 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        lookup_table = {}
+        hashmap = {}
         
-        for index, value in enumerate(nums):
-            complement = target - nums[index]
+        for i in range(len(nums)):
+            number_two = target - nums[i]
             
-            if complement in lookup_table:
-                return [index, lookup_table[complement]]
+            if number_two in hashmap:
+                return [i, hashmap[number_two]]
             
-            lookup_table[nums[index]] = index
+            hashmap[nums[i]] = i
+            
